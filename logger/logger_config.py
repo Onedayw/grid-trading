ed7718da8f738466f67f4ff5f73287104f061454
Nate Wang <ytowang@ucdavis.edu>
2023-07-02 22:50:33 -0700

    Update architecture

---
+++ logger/logger_config.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+from datetime import datetime
+td = datetime.today().date()
+
+logging.basicConfig(filename=f'mylog_{td}.log', format='%(asctime)s - %(message)s')
+logger = logging.getLogger()
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stdout_handler)
